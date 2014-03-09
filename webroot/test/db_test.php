24b6e0735a9d36b9b9bf0a27304427d0a42706fd
Jesse <jesse_108@163.com>
2014-03-09 15:16:32 +0800

    add test

--- webroot/test/db_test.php
+++ webroot/test/db_test.php
@@ -1,3 +1,25 @@
 <?php
 include_once dirname(dirname(dirname(__FILE__))).'/app.php';
 DB::Debug();
+
+
+$inserUser = array(
+	'id' => 1,
+	'name' => 'jesse',
+	'sex' => '1',
+);
+
+$insertID = DB::Insert('user', $inserUser);
+
+$delContion = array(
+	'id' => 1,
+);
+
+$condition = array(
+	'id' => 1,
+);
+
+$option = array(
+	'order' => 'order bu id desc',
+);
+$result = DB::LimitQuery('user',$condition);
